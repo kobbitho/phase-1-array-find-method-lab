fe278797da0ffdf99d86eab2d9549d29e10c7cb6
Tabithaokoth <kobbitho@gmail.com>
2024-03-26 21:41:12 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// code your solution here
+// code your solution here...
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  
+]
+function superbowlWin(recordsArray){ //declare the function
+    let response = "";// define response
+    response = recordsArray.find(object => (object.result === "W"));// use array.find to loop through the objects and fing the one with the rsult "W".
+    //response = (response === undefined) ? undefined : response.year;  //use of tanary
+
+if (response ===undefined){
+    response = undefined;
+}else{
+    return response.year;
+}
+return response;
+}      //use if else
+
+console.log(superbowlWin(record));
